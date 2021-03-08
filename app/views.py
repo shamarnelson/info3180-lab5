ceab5339a1b31c2b3a1c698659ba4eb1ecaf77af
shamarnelson <50060085+shamarnelson@users.noreply.github.com>
2021-03-08 17:31:11 -0500

    check over -_-

--- app/views.py
+++ app/views.py
@@ -10,6 +10,7 @@ from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, current_user, login_required
 from app.forms import LoginForm
 from app.models import UserProfile
+from werkzeug.security import check_password_hash
 
 
 ###
@@ -31,7 +32,7 @@ def about():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
-    if request.method == "POST":
+    if request.method == "POST" and form.validate_on_submit():
         # change this to actually validate the entire form submission
         # and not just one field
         if form.username.data:
@@ -42,13 +43,32 @@ def login():
             # You will need to import the appropriate function to do so.
             # Then store the result of that query to a `user` variable so it can be
             # passed to the login_user() method below.
+            username = form.username.data
+            password = form.password.data
+            user=UserProfile.query.filter_by(username=username).first()
+            if user is not None and check_password_hash(user.password,password):
 
             # get user id, load into session
-            login_user(user)
+                login_user(user)
+                flash('Logged in successfully.','success')
+                return redirect(url_for("secure_page"))
 
             # remember to flash a message to the user
+            else:
+                flash('Username or Password is incorrect.','danger')
             return redirect(url_for("home"))  # they should be redirected to a secure-page route instead
     return render_template("login.html", form=form)
+
+@app.route('/secure-page')
+@login_required
+def secure_page():
+    return render_template('secure_page.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 
 # user_loader callback. This callback is used to reload the user object from
